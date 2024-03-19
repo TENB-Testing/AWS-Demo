acd5ca591759661da283d5623de0fe9e3ab515da
fpiquette <76015308+fpiquette@users.noreply.github.com>
2024-03-19 05:36:33 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+#AWS Config
+
+variable "aws_access_key" {
+  default = "AKIAQRFCXDD3XWULIOPL"
+}
+
+variable "aws_secret_key" {
+  default = "Q3GKBm8UC6ht1w5DoroEwXnYmTFyc8BRqDz3GBRz"
+}
+  
+variable "aws_region" {
+  default = "us-east-1"
+}
